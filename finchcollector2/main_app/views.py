3ffeed2e16ccd6e18d32265984b9ad39a952d978
Duane Wellington <dwelling77@gmail.com>
2024-01-10 11:18:51 -0500

    part 1 complete

--- finchcollector2/main_app/views.py
+++ finchcollector2/main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'Flicker', 'breed': 'Shaft-tail', 'description': 'never idle, always moving', 'age': 2},
+  {'name': 'Runnymede', 'breed': 'European Goldfinch', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 
 def home(request):
@@ -9,4 +14,5 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
-    return render(request, 'finches/index.html', {'finches': finches})
+    return render(request, 'finches/index.html', {
+        'finches': finches})
